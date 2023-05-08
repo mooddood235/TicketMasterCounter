74a66ecbb1770f1146c95e14dda0608f0b58f164
Mahmoud Zeidan <62807754+mooddood235@users.noreply.github.com>
2023-05-07 22:02:48 -0400

    Add files via upload

--- TicketMasterCounter.py
+++ TicketMasterCounter.py
@@ -13,21 +13,45 @@ def get_data():
     f.close()
     return data
 
+def get_blocked():
+    blocked = input("Enter blocked sections seperated by spaces: ")
+    return blocked.split(" ")
+
+def get_block_name(block: str):
+    data_index = block.find('data-section-name="')
+    data_index += len('data-section-name="')
+
+    data = ''
+
+    while block[data_index] != '"':
+        data += block[data_index]
+        data_index += 1
+
+    return data
+
 def main():
     data = get_data()
-    seats = re.findall(r'data-component="svg__seat"[^(class)]*class="[^"]*"', data)
-    
+    blocked = get_blocked()
+
+    data = data[data.find('<g class="seats">') + len('<g class="seats">'): data.find('<g class="polygons">')]
+    blocks = re.split(r'data-component="svg__block"', data)[1:]
+
     is_unavailable = 0
     is_available = 0
     is_resale = 0
 
-    for seat in seats:
-        if "is-resale" in seat:
-            is_resale += 1
-        elif "is-available" in seat:
-            is_available += 1
-        else:
-            is_unavailable += 1
+    for block in blocks:
+        if get_block_name(block) in blocked:
+            continue
+        seats = re.findall(r'data-component="svg__seat"[^(class)]*class="[^"]*"', block)
+
+        for seat in seats:
+            if "is-resale" in seat:
+                is_resale += 1
+            elif "is-available" in seat:
+                is_available += 1
+            else:
+                is_unavailable += 1
 
     print("Unavailable:", is_unavailable)
     print("Available:", is_available)
